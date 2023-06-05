f599fbc36bd08b0372eae9c6d4d51c9473891526
Julien <parsy.julien@hotmail.fr>
2023-06-05 15:05:40 +0200

    Premier commit
    Création du repo

---
+++ variables.py
@@ -0,0 +1,67 @@
+# Nombre entier, integer (int)
+number1 = 123
+number1 = 42
+print(number1)
+
+# nombre à virgule flottante, float (float)
+number2 = 3.14
+number2 = 2.71
+print(number2)
+
+# chaine de caractères, string (str)
+text1 = 'foo bar baz'
+print(text1)
+
+text2 = "foo bar baz"
+print(text2)
+
+# booléen, boolean (bool)
+python_is_cool = True
+print(python_is_cool)
+
+python_is_boring = False
+print(python_is_boring)
+
+# valeur nulle, null value (noneType)
+user_accepted_terms = None
+print(None)
+
+#types de données
+print (type(number1))
+print (type(number2))
+print (type(text1))
+print (type(text2))
+print (type(python_is_cool))
+print (type(python_is_boring))
+print (type(user_accepted_terms))
+
+# vérification du type de données
+print (type(number1) is int)
+print (type(number1) is str)
+
+# to do: intérroger le type des autres variables...
+
+# transtypage int -> str
+print(type(str(number1)))
+print(str(number1))
+
+# transtypage int-> bool
+print(type(bool(number1)))
+print(bool(number1))
+
+# convertie en booléen la valeur 0 donne  False
+number3 = 0
+print((bool(number3)))
+
+#transtypage str -> int génère une erreur
+# il ne peut pas y avoir autre chose qu'un nombre
+# dans la chaine de caractères 
+# si on veut la converir en int
+text3 = "123456789"
+print(type(int(text3)))
+
+# les fonctions de transtypage
+# str() convertit vers string
+# int() convertit vers integer
+# float() convertit vers float
+# bool ( convertit vers boolean)
