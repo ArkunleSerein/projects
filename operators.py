b1f79349a9a3003dcb1c3b34f91bd4e58116723e
Julien <parsy.julien@hotmail.fr>
2023-06-06 16:05:33 +0200

    Cours operators

---
+++ operators.py
@@ -0,0 +1,67 @@
+# arithmétique
+a = 123 + 42
+a = 123 - 42
+a = 123 * 42
+a = 123 / 42
+
+# division entière, integer division
+a = 42 // 123
+print(a)
+
+# modulo ou reste de la division (euclidienne)
+a = 53
+# si le reste, le nombre n'est pas divisible par deux, donc il n'est pas pair
+print(a % 2)
+
+a = 74
+# s'il y a un reste, le nombre n'est pas divisible par 2 donc il n'est pas pair
+print(a % 2)
+
+# puissance, exponentiation
+# deux puissance tois
+a = 2**3
+print(a)
+
+# opérateur de comparaison
+# opérateur d'égalité
+result = 123 == 42
+print(result)
+print(123 == 123)
+print(123 == 42)
+
+password = "abc"
+user_input = "def"
+print(password == user_input)
+# différent de
+print(123 != 42)
+
+# (strictement) plus grand que
+print(123 > 42)
+
+# plus grand ou égal à
+print(42 >= 42)
+
+# (strictement) plus petit que
+print(42 > 123)
+
+# plus petit ou égal à 
+print(42 <= 42)
+
+# comparaison d'égalité
+# renvoie true
+
+console.log("hello JS !");
+let number1 = 123;
+let text1 = "123";
+
+# comparaison d'égalité
+# renvoie true
+console.log (number1 == text1);
+console.log (tex1 == number1);
+
+# comparaison d
+# renvoie false
+console.log (number1 === text1);
+console.log (text1 === number1);
+
+
