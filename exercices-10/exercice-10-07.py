4a56ce778f3045d7869980bf56bb86a3ad7dc4ad
Julien <parsy.julien@hotmail.fr>
2023-06-28 14:01:34 +0200

    exercice 10.7 effectué

--- exercices-10/exercice-10-07.py
+++ exercices-10/exercice-10-07.py
@@ -13,3 +13,25 @@
 
 # réponse 10.7
 
+def compute_tax(price: float, tax_type: int) -> float:
+    if tax_type == 1:
+        tax_rate = 2.1
+    elif tax_type == 2:
+        tax_rate = 5.5
+    elif tax_type == 3:
+        tax_rate = 10
+    elif tax_type == 4:
+        tax_rate = 20
+    else:
+        return price
+    
+    tax_amount = price * (tax_rate / 100)
+    total_price = price + tax_amount
+    return total_price
+
+# Test avec un montant de 100 € pour chaque type de taxe
+prix = 100
+
+for tax_type in range(1, 5):
+    total_price = compute_tax(prix, tax_type)
+    print(f"Type de taxe {tax_type}: Prix TTC = {total_price} €")
